e73d5dc49fa0e457e433821b346fb1170c5b8426
drabard <palusba@gmail.com>
2022-01-19 19:05:56 +0100

    course finished

--- Social network database/social_queries3.sql
+++ Social network database/social_queries3.sql
@@ -3,18 +3,22 @@ DELETE FROM highschooler
 WHERE grade = 12;
 
 /*If two students A and B are friends, and A likes B but not vice-versa, remove the Likes tuple.*/
-DELETE FROM likes
-WHERE ID1 IN 
-(SELECT likes.ID1 FROM friend, likes WHERE CONCAT(CAST(friend.ID1 AS CHAR(5)), CAST(friend.ID2 AS CHAR(5))) = CONCAT(CAST(likes.ID1 AS CHAR(5)), CAST(likes.ID2 AS CHAR(5))));
-
-/*helper code*/
-SELECT likes.ID1 FROM friend, likes WHERE CONCAT(CAST(friend.ID1 AS CHAR(5)), CAST(friend.ID2 AS CHAR(5))) = CONCAT(CAST(likes.ID1 AS CHAR(5)), CAST(likes.ID2 AS CHAR(5)))
-;  
-SELECT likes.ID1 FROM friend, likes WHERE CONCAT(CAST(friend.ID2 AS CHAR(5)), CAST(friend.ID1 AS CHAR(5))) = CONCAT(CAST(likes.ID1 AS CHAR(5)), CAST(likes.ID2 AS CHAR(5)));
-
-DELETE FROM likes
-WHERE ID1 IN 
-(SELECT likes.ID1 FROM friend, likes WHERE CAST(friend.ID1 AS CHAR(5)) || CAST(friend.ID2 AS CHAR(5)) = CAST(likes.ID1 AS CHAR(5)) || CAST(likes.ID2 AS CHAR(5)))
-AND likes.ID1 <> likes.ID2;
-
-SELECT CAST(friend.ID1 AS CHAR(5)) || CAST(friend.ID2 AS CHAR(5)) FROM friend, likes
+DELETE FROM Likes
+WHERE ID1 IN (SELECT L.ID1 
+	FROM Likes L JOIN Friend F
+	USING (ID1)
+	WHERE L.ID2 = F.ID2)
+AND ID2 NOT IN(SELECT L.ID1 
+	FROM Likes L JOIN Friend F
+	USING (ID1)
+	WHERE L.ID2 = F.ID2);
+    
+  /*For all cases where A is friends with B, and B is friends with C, add a new friendship for the pair A and C. 
+  Do not add duplicate friendships, friendships that already exist, or friendships with oneself. 
+  (This one is a bit challenging; congratulations if you get it right.)*/  
+INSERT INTO friend
+SELECT f1.id1, f2.id2
+FROM friend f1 JOIN friend f2 ON f1.id2 = f2.id1
+WHERE f1.id1 <> f2.id2
+EXCEPT
+SELECT * FROM friend;
